eea419800f3841051ed239a61c3630d942533456
rizwanch786 <rizwangcsit@gmail.com>
2022-05-19 14:37:37 +0500

    ORM

---
+++ generic_views/api/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView,RetrieveAPIView,RetrieveDestroyAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView,ListAPIView,DestroyAPIView, UpdateAPIView
+# Create your views here.
